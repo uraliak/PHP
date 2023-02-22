f4339045e99e8db0bbc7ab5ec5e6d82d8b9cbade
uraliak <uraliak@mail.ru>
2023-02-22 20:08:09 +0300

    made form

---
+++ Lab2/index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <header class="header">
+        <div class="header__wrapper">
+            <a href="" class="header__logo logo"><img src="./img/logo.jpg" alt="" class="logo__image" width="300" height="90"></a>
+            <h1 class="header__heading">2.1.Домашняя работа: Hello, World!</h1>
+        </div>
+    </header>
+    <main>
+      <div class="container">
+        <form method="POST" action="back.php">
+          <legend>Feedback form</legend>
+          <div class="mb-3">
+            <label for="disabledTextInput" class="form-label">Name</label>
+            <input type="text" name="name" class="form-control" placeholder="Enter your name">
+          </div>
+          <div class="mb-3">
+            <label for="disabledTextInput" class="form-label">E-mail</label>
+            <input type="text" name="e-mail" class="form-control" placeholder="Enter your e-mail">
+          </div>
+          <div class="mb-3">
+            <label for="disabledSelect" class="form-label">Type</label>
+            <select name="select" class="form-select">
+              <option name="good">Good</option>
+              <option name="bad">Жалоба</option>
+              <option name="qustions">Вопрос</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <textarea name="text" id="" cols="30" rows="10"></textarea>
+          </div>
+          <div class="mb-3">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" name="sms">
+              <label class="form-check-label" for="disabledFieldsetCheck">sms</label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" name="check-email">
+              <label class="form-check-label" for="disabledFieldsetCheck">e-mail</label>
+            </div>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </main>
+    <footer>
+        <div class="footer__wrapper">
+            <a href="" class="header__logo logo"><img src="./img/logo.jpg" alt="" class="logo__image" width="300" height="90"></a>
+        </div>
+    </footer>
+  </body>
+</html>
