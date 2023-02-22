75f6a976cdd43c635c1e057fd728370ed1af4300
uraliak <uraliak@mail.ru>
2023-02-22 18:50:47 +0300

    world.php передача

---
+++ world.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Киньябулатова Уралия, 221-321</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header class="header">
+        <div class="header__wrapper">
+            <a href="" class="header__logo logo"><img src="./img/logo.jpg" alt="" class="logo__image" width="300" height="90"></a>
+            <h1 class="header__heading">2.1.Домашняя работа: Hello, World!</h1>
+        </div>
+    </header>
+    <main>
+        <div class="main__wrapper">
+            <?php echo '<h2 align="center">Hello, world!</h1>'; ?>
+        </div>
+    </main>
+    <footer>
+        <div class="footer__wrapper">
+            <a href="" class="header__logo logo"><img src="./img/logo.jpg" alt="" class="logo__image" width="300" height="90"></a>
+        </div>
+    </footer>
+</body>
+</html>
