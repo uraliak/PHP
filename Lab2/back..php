c469b3d40c2f86a32fd9bfd54d8a4ab105e30cb3
uraliak <uraliak@mail.ru>
2023-02-22 22:01:37 +0300

    work in process

--- Lab2/back..php
+++ Lab2/back..php
@@ -16,7 +16,13 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <main>
-        <textarea name="" id="" cols="70" rows="80"><?php print_r(get_headers('https://getbootstrap.com/docs/5.3/forms/overview/#form-text'))?></textarea>
+        <div class="get-container">
+            <textarea name="text" id="" cols="70" rows="70">
+                <?php
+                    print_r(get_headers('https://getbootstrap.com/docs/5.3/forms/overview/#disabled-forms'));
+                ?>
+            </textarea>
+        </div>
     </main>
   </body>
 </html>
