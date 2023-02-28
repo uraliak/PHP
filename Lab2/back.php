8cd7da80fcb081f322d8ea4c2e7560ea62d40472
uraliak <uraliak@mail.ru>
2023-02-28 14:36:03 +0300

    Произведена доработка всех имеющихся недостатков

--- Lab2/back.php
+++ Lab2/back.php
@@ -16,13 +16,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <main>
-        <div class="get-container">
-            <textarea name="text" id="" cols="70" rows="70">
-                <?php
-                    print_r(get_headers('https://getbootstrap.com/docs/5.3/forms/overview/#overview'));
-                ?>
-            </textarea>
-        </div>
+      <textarea class="form-control" name="form-control" id="" rows="3">
+        <?php print_r(get_headers('https://getbootstrap.com/docs/5.3/forms/overview/#overview')); ?>
+      </textarea>
     </main>
   </body>
 </html>
